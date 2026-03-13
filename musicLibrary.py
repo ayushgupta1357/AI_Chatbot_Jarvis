9428c1777252b460fc9c00ec728de4612680f754
Ayush Kumar Gupta <www.ayushgupta1234@gmail.com>
2026-03-13 13:44:27 +0530

    Delete musicLibrary.py

--- musicLibrary.py
+++
@@ -1,7 +0,0 @@
-musicLinks={
-    'aadat':'https://www.youtube.com/watch?v=lcw4TDMBjZA&list=RDlcw4TDMBjZA&start_radio=1&pp=ygUFc29uZ3OgBwHSBwkJxQoBhyohjO8%3D',
-    'hangover':"https://www.youtube.com/watch?v=O-I1VniQcvA&list=RDO-I1VniQcvA&start_radio=1&pp=ygUFc29uZ3OgBwHSBwkJxQoBhyohjO8%3D",
-    'sapphire':'https://www.youtube.com/watch?v=JgDNFQ2RaLQ&list=RDJgDNFQ2RaLQ&start_radio=1&pp=ygUFc29uZ3OgBwE%3D',
-    'beautiful':'https://www.youtube.com/watch?v=QI40p9zhX2w&list=RDQI40p9zhX2w&start_radio=1&pp=ygUFc29uZ3OgBwE%3D',
-}
-
